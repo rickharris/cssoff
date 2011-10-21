c3f3bca7ace3ec57b2bdbf78b56de2db9b74e28b
Rick Harris <rick.c.harris@gmail.com>
2011-10-21 11:19:05 -0400

    Add typographic styles

--- config.rb
+++ config.rb
@@ -6,6 +6,7 @@ css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "js"
+fonts_dir = "fonts"
 
 # You can select your preferred output style here (can be overridden via the command l
 # output_style = :expanded or :nested or :compact or :compressed

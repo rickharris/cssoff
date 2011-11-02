8e3edf692726b4a613d9810b2b65f43dc8d82d15
Rick Harris <rick.c.harris@gmail.com>
2011-11-01 23:38:09 -0400

    Obstacle animations

--- js/application.js
+++ js/application.js
@@ -1,4 +1,42 @@
 (function($) {
-  $('select').uniform();
-  $('label').inFieldLabels();
+
+  $(document).ready(function() {
+
+    var obstacleWrapper = $('#obstacles');
+
+    // Form enhancements
+    $('select').uniform();
+    $('label').inFieldLabels();
+
+    // Obstacle show/hide
+    $('#obstacles').find('nav a').click(function(e) {
+
+      var obstacleChoice = $(this);
+
+      e.preventDefault();
+
+      // Only act if this link isn't already the active one
+      if (!obstacleChoice.hasClass('active')) {
+
+        obstacleWrapper
+          // remove any active classes
+          .find('.active').removeClass('active')
+          // the 'hidden' class is required for the exit animation and is
+          // applied to the previously active obstacle
+          .filter('.obstacle').addClass('hidden');
+
+        // Find the obstacle that this link corresponds to and show it
+        $('#' + obstacleChoice.attr('class'))
+          .removeClass('hidden')
+          .addClass('active');
+
+        // Finally, show this link as being the selected one
+        obstacleChoice.addClass('active');
+
+      }
+
+    });
+
+  });
+
 })(jQuery);

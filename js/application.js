73c2de59186f5912881347f8cf747e1a35ad8731
Rick Harris <rick.c.harris@gmail.com>
2011-11-03 12:59:08 -0400

    Wrap all javascript functionality in a check for legacy browsers

--- js/application.js
+++ js/application.js
@@ -7,114 +7,117 @@
     var mainNav = $('#top nav');
     var clock = $('#clock');
 
-    // Form enhancements
-    // -----------------
-
     if (!isLegacy) {
+
+      // Form enhancements
+      // -----------------
+
       $('select').uniform();
       $('label').inFieldLabels({
         fadeOpacity: 0.7,
         fadeDuration: 200
       });
-    }
 
-    // Obstacle show/hide
-    // ------------------
+      // Obstacle show/hide
+      // ------------------
 
-    $('#obstacles').find('nav a').click(function(e) {
+      $('#obstacles').find('nav a').click(function(e) {
 
-      var obstacleChoice = $(this);
+        var obstacleChoice = $(this);
 
-      e.preventDefault();
+        e.preventDefault();
 
-      // Only act if this link isn't already the active one
-      if (!obstacleChoice.hasClass('active')) {
+        // Only act if this link isn't already the active one
+        if (!obstacleChoice.hasClass('active')) {
 
-        obstacleWrapper
-          // remove any active classes
-          .find('.active').removeClass('active')
-          // the 'hidden' class is required for the exit animation and is
-          // applied to the previously active obstacle
-          .filter('.obstacle').addClass('hidden');
+          obstacleWrapper
+            // remove any active classes
+            .find('.active').removeClass('active')
+            // the 'hidden' class is required for the exit animation and is
+            // applied to the previously active obstacle
+            .filter('.obstacle').addClass('hidden');
 
-        // Find the obstacle that this link corresponds to and show it
-        $('#' + obstacleChoice.attr('class'))
-          .removeClass('hidden')
-          .addClass('active');
+          // Find the obstacle that this link corresponds to and show it
+          $('#' + obstacleChoice.attr('class'))
+            .removeClass('hidden')
+            .addClass('active');
 
-        // Finally, show this link as being the selected one
-        obstacleChoice.addClass('active');
+          // Finally, show this link as being the selected one
+          obstacleChoice.addClass('active');
 
-      }
-    });
+        }
+      });
 
-    // Dynamic menu position
-    // ---------------------
+      // Dynamic menu position
+      // ---------------------
 
-    // Trigger the navigation to move whenever we scroll past the bottom of the
-    // navigation
-    $('#top').waypoint(function(e, dir) {
+      // Trigger the navigation to move whenever we scroll past the bottom of the
+      // navigation
+      $('#top').waypoint(function(e, dir) {
 
-      // Set the nav to jump to the top of the page by default (i.e. scrolling
-      // up)
-      var topOffset = 0;
+        // Set the nav to jump to the top of the page by default (i.e. scrolling
+        // up)
+        var topOffset = 0;
 
-      // If we're scrolling down, we want the nav to jump to the bottom of the
-      // header
-      if (dir === 'down') {
-        topOffset = $(this).height() - mainNav.height();
-      }
+        // If we're scrolling down, we want the nav to jump to the bottom of the
+        // header
+        if (dir === 'down') {
+          topOffset = $(this).height() - mainNav.height();
+        }
 
-      mainNav.css('top', topOffset);
+        mainNav.css('top', topOffset);
 
-    }, { offset: -mainNav.height() });
+      }, { offset: -mainNav.height() });
 
-    // Trigger the navigation to move whenever we scroll past the top of the
-    // first two sections
-    $('#obstacles, #prizes').waypoint(function(e, dir) {
+      // Trigger the navigation to move whenever we scroll past the top of the
+      // first two sections
+      $('#obstacles, #prizes').waypoint(function(e, dir) {
 
-      // If scrolling up, we want to set the navigation right above the top of
-      // the section
-      var topOffset = $(this).offset().top;
+        // If scrolling up, we want to set the navigation right above the top of
+        // the section
+        var topOffset = $(this).offset().top;
 
-      // If scrolling down, we want the navigation to jump to right above the
-      // next section
-      if (dir === 'down') {
-        topOffset = $(this).next().offset().top;
-      }
+        // If scrolling down, we want the navigation to jump to right above the
+        // next section
+        if (dir === 'down') {
+          topOffset = $(this).next().offset().top;
+        }
 
-      mainNav.css('top', topOffset - mainNav.height());
-    });
+        mainNav.css('top', topOffset - mainNav.height());
+      });
 
 
-    // Clock countdown
-    // ---------------
-    
-    (function countDown() {
+      // Clock countdown
+      // ---------------
+      
+      (function countDown() {
 
-      function reset() {
-        clock.removeClass('alert').text('60');
+        function reset() {
+          clock.removeClass('alert').text('60');
+          setTimeout(decrement, 1000);
+        }
+
+        function decrement() {
+          var seconds = parseInt(clock.text());
+          clock.text(--seconds);
+
+          // If the time is positive, keep countin
+          if (seconds > 0) {
+            countDown();
+          }
+          // If we've reached zero, delay and show the alert state
+          else {
+            clock.addClass('alert');
+            setTimeout(reset, 5000);
+          }
+        }
+
         setTimeout(decrement, 1000);
-      }
 
-      function decrement() {
-        var seconds = parseInt(clock.text());
-        clock.text(--seconds);
+      })();
 
-        // If the time is positive, keep countin
-        if (seconds > 0) {
-          countDown();
-        }
-        // If we've reached zero, delay and show the alert state
-        else {
-          clock.addClass('alert');
-          setTimeout(reset, 5000);
-        }
-      }
+    } // end if isLegacy
 
-      setTimeout(decrement, 1000);
-
-    })();
-  });
+  }); // end document.ready
 
 })(jQuery);
